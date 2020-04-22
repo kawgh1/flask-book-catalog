2667fe4b4c557bcdf693877812b94203446b52c3
thrwwy216 <thrwwy216@tutanota.com>
2020-04-22 01:53:20 -0500

    made corrections

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from bookFlask import create_app, db
 from bookFlask.auth.models import User
+from sqlalchemy import exc
 
 
 # This is where we run the app. It is outside of the bookFlask folder
 # by importing the create_app function, we can easily switch between 'dev', 'test' and 'prod'
 # runtimes with a quick change here
 
-if __name__ == '__main__':
+# The next line was removed because it may not be relevant when we upload to webserver
+# if __name__ == '__main__':
 
     # 'flask_app' below is used in the Procfile
     flask_app = create_app('prod')
@@ -16,11 +18,13 @@ if __name__ == '__main__':
 
         # this code says, if user_name 'harry' does not exist in our database
         # create that user
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry',
+        try:
+            if not User.query.filter_by(user_name='harry').first():
+                User.create_user(user='harry',
                              email='harry@potters.com',
                              password='secret')
-    flask_app.run()
+        except exc.IntegrityError:
+            flask_app.run()
 
 
 # In order to upload to Heroku we need to install 'gunicorn' which is a Python HTTP server
