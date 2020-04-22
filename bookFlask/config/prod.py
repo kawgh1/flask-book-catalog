548bf01b9801786c5c30dc8a07f3512d9b1d46f8
thrwwy216 <thrwwy216@tutanota.com>
2020-04-22 10:57:38 -0500

    fixed some spacing

--- bookFlask/config/prod.py
+++ bookFlask/config/prod.py
@@ -2,7 +2,7 @@ import os
 
 
 DEBUG = False
-SECRET_KEY='asdf4809uasd908r3q450a987fsdf9043j6l23io6u2nhvgbpo932u5gasdf2q345bhwqe'
+SECRET_KEY = 'asdf4809uasd908r3q450a987fsdf9043j6l23io6u2nhvgbpo932u5gasdf2q345bhwqe'
 # SQLALCHEMY_DATABASE_URI='<database>://<user_id>:<password>@<server>/<database_name>'
 
 # The next line is only relevant for a localhost website,
@@ -11,7 +11,7 @@ SECRET_KEY='asdf4809uasd908r3q450a987fsdf9043j6l23io6u2nhvgbpo932u5gasdf2q345bhw
 # since we are hosting on the web, we need a different DATABASE_URI
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # since these changes were made they were commited to both the local and remote repositories
 # 'git add .'
