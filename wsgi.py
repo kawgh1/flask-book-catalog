b0a5d394826cc102d3df97e5791e8e9a49ccba7f
thrwwy216 <thrwwy216@tutanota.com>
2020-04-23 13:48:01 -0500

    again

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,23 @@
-from bookFlask import create_app
+from bookFlask import create_app, db
+from bookFlask.auth.models import User
 
-app = create_app()
+# app = create_app()
+
+flask_app = create_app('dev')
+# spin up the database and tables/models from the current app above
+with flask_app.app_context():
+
+    db.create_all()
+
+    # this code says, if user_name 'harry' does not exist in our database
+    # create that user
+    # try:
+    #     if not User.query.filter_by(user_name='harry').first():
+    #         User.create_user(user='harry', email='harry@potters.com', password='secret')
+    # except exc.IntegrityError:
+    #     flask_app.run()
+
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harry@potters.com', password='secret')
+
+flask_app.run()
